b327fbb45ee0dfea5d2bf956ff5b284d5c3290f7
Kelcey Swain <kelceyswain@users.noreply.github.com>
2022-05-10 15:14:51 +0100

    Initial commit
    No Cooley-Tukey yet but a working DFT iDFT class.

---
+++ DFT.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Numerics;
+
+public class DFT
+{
+	public double[] realValues;
+	public Complex[] complexValues;
+    public double[] outValues;
+    private double twopi = Math.PI * 2.0;
+
+	public DFT(double[] inputArray)
+	{
+		realValues = inputArray;
+        complexValues = new Complex[realValues.Length];
+        outValues = new double[realValues.Length];
+	}
+
+	public void ForwardDFT()
+    {        
+        int N = realValues.Length;
+        // temporary buffer
+        Complex[] X = new Complex[N];
+
+        if (N <= 0)
+        {
+            throw new IndexOutOfRangeException("Invalid signal length.");
+        }
+        // Is the array size a power of two?
+        if ((N & (N - 1)) != 0)
+        {
+            throw new ArgumentException("Signal size must be a power of 2, or zero padded.");
+        }
+        // We really only need (N/2)+1 because it is a 1d array, and therefore reflects
+        for (int i=0; i<N; i++)
+        {
+            X[i] = new Complex(0, 0);
+            for (int n=0; n<N; n++)
+            {
+                double realPart = realValues[n] * Math.Cos(i*n*twopi/N);
+                double imagPart = -1 * realValues[n] * Math.Sin(i*n*twopi/N);
+                X[i] += new Complex(realPart, imagPart);
+            }
+            X[i] = X[i] / N;
+        }
+		complexValues = X;
+    }
+
+	public void InverseDFT()
+    {
+        // Must have made the complex values first!
+        int N = realValues.Length;
+        // Temporary buffer
+        double[] workingOut = new double[N];
+        
+        for (int i=0; i<N; i++)
+        {
+            workingOut[i] = 0.0;
+            for (int n=0; n<N; n++)
+            {
+                double realPart = complexValues[n].Real * Math.Cos(i*n*twopi/N);
+                double imagPart = complexValues[n].Imaginary * Math.Sin(i*n*twopi/N);
+                workingOut[i] += realPart - imagPart;
+            }
+        }
+        // And set the final values;
+        outValues = workingOut;
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿// A simple Hann window
+double[] HannWindow(double[] inputArray)
+{
+    double[] outputArray = new double[inputArray.Length];
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        double hannFactor = 0.5 * (1 - Math.Cos(2 * Math.PI * i / (inputArray.Length - 1)));
+        outputArray[i] = hannFactor * inputArray[i];
+    }
+    return outputArray;
+}
+
+double CompareSignals(double[] signalInput, double[] signalOutput)
+{
+    if (signalInput.Length != signalOutput.Length)
+    {
+        throw new ArgumentException("Arrays must be equally shaped");
+    }
+    double error = 0.0;
+    for (int i = 0; i < signalInput.Length; i++)
+    {
+        error += Math.Abs(signalInput[i] - signalOutput[i]);
+    }
+    // Normalise it to get average error
+    return error/(double)signalInput.Length;
+}
+
+Console.WriteLine("A Simple test to perform the Cooley-Tukey DFT algorithm.");
+// Instantiate Random
+Random rand = new Random();
+// Create two 2**n sample buffer
+int sampleCount = 1024;
+double[] inputSamples = new double[sampleCount];
+
+for (int i = 0; i < inputSamples.Length; i++)
+{
+    inputSamples[i] = rand.NextDouble();
+}
+
+// Window the samples
+inputSamples = HannWindow(inputSamples);
+
+Console.WriteLine(String.Join("\t", inputSamples));
+
+DFT dft = new DFT(inputSamples);
+
+// Perform the DFT
+dft.ForwardDFT();
+Console.WriteLine("======================");
+Console.WriteLine(String.Join("\t", dft.complexValues));
+
+// Perform the iDFT
+dft.InverseDFT();
+Console.WriteLine("======================");
+Console.WriteLine(String.Join("\t", dft.outValues));
+
+// Add up the errors and see how we did
+double e = CompareSignals(inputSamples, dft.outValues);
+Console.WriteLine(e);
